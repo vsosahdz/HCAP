2ef8e2cfe53301f25b990df93adce8313a56d27f
vsosahdz <vsosa@tec.mx>
2020-10-29 13:03:17 -0600

    Agregar nuevas funcionalidades y archivo

--- ejemplo1.py
+++ ejemplo1.py
@@ -3,12 +3,20 @@ import cv2
 imagen = cv2.imread('imagen.jpg')
 imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB)
 print(imagen.shape)
-print(imagen[0][0][0])
+print(imagen[0][0])
 imagen = cv2.resize(imagen,(256,256))
 
 cv2.imwrite('resizeimagen.jpg',imagen)
+
+
+imagen = cv2.imread('imagen.jpg')
+imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+print(imagen.shape)
+print(imagen[0][0])
+cv2.imwrite('grayimagen.jpg',imagen)
+
 #Only native linux and Mac
-cv2.imshow('image',imagen)
-cv2.waitKey(0)
+#cv2.imshow('image',imagen)
+#cv2.waitKey(0)
 
 
